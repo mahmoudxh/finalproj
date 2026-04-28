6cb2f16560ba8cf659862a83d25e94593457ef6a
Mahmoud <you@example.com>
2026-04-28 11:45:21 +0300

    fix cartContext type

--- src/app/_contexts/CartContextProvider.tsx
+++ src/app/_contexts/CartContextProvider.tsx
@@ -1,37 +1,37 @@
 "use client";
 
-import React, { createContext, ReactNode, useEffect, useState } from "react";
-import { getLoggedUser } from "../_actions/cart.Action";
+import React, { createContext, ReactNode, useState } from "react";
 import { cartItemType, cartResType } from "@/types/cart.type";
 import { UserWhishlistResType } from "@/types/whishlist.type";
 
+type CartContextType = {
+    cartId: string;
+    cartItemsNum: number;
+    setcartItemsNum: React.Dispatch<React.SetStateAction<number>>;
+    totalPriceOfCart: number;
+    settotalPriceOfCart: React.Dispatch<React.SetStateAction<number>>;
+    cartProducts: cartItemType[];
+    setcartProducts: React.Dispatch<React.SetStateAction<cartItemType[]>>;
+    whishlistItemsNum: number;
+    setwhishlistItemsNum: React.Dispatch<React.SetStateAction<number>>;
+    whishlistItems: UserWhishlistResType["data"];
+    setwhishlistItems: React.Dispatch<React.SetStateAction<UserWhishlistResType["data"]>>;
+}
 
-export const cartContext = createContext({});
-export default function CartContextProvider( {children , userCart, userWishlist }: {children: ReactNode , userCart : cartResType, userWishlist : UserWhishlistResType}) {
-    
+export const cartContext = createContext<CartContextType>({} as CartContextType);
+
+export default function CartContextProvider({ children, userCart, userWishlist }: { children: ReactNode, userCart: cartResType, userWishlist: UserWhishlistResType }) {
+
     const [cartId, setcartId] = useState(userCart?.cartId)
     const [cartItemsNum, setcartItemsNum] = useState<number>(userCart?.numOfCartItems);
     const [totalPriceOfCart, settotalPriceOfCart] = useState<number>(userCart?.data?.totalCartPrice);
     const [cartProducts, setcartProducts] = useState<cartItemType[]>(userCart?.data?.products);
     const [whishlistItemsNum, setwhishlistItemsNum] = useState<number>(userWishlist?.count)
     const [whishlistItems, setwhishlistItems] = useState(userWishlist?.data)
-    
-    
-    // async function getloggedUserFromApi() {
-    //     const userCart = await getLoggedUser();
-    //     setuserCartData(userCart)
-    //     setcartItemsNum(userCart.numOfCartItems)
-    //     settotalPriceOfCart(userCart.data.totalCartPrice)
-    //     setcartProducts(userCart.data.products)
-    // }
 
-    // useEffect(() => {
-    //     getloggedUserFromApi();
-    // }, []);
-    
     return (
         <cartContext.Provider value={{ cartItemsNum, setcartItemsNum, totalPriceOfCart, settotalPriceOfCart, cartProducts, setcartProducts, cartId, whishlistItemsNum, setwhishlistItemsNum, whishlistItems, setwhishlistItems }}>
-        {children}
+            {children}
         </cartContext.Provider>
     );
 }
