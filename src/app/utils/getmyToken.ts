ca05e3218b140aabdb614fdeab21b73b75fb800a
Mahmoud <you@example.com>
2026-05-03 14:09:08 +0300

    fix null check in getmyToken

--- src/app/utils/getmyToken.ts
+++ src/app/utils/getmyToken.ts
@@ -1,16 +1,16 @@
 import { decode } from 'next-auth/jwt'
 import { cookies } from 'next/headers'
 
-import React from 'react'
-
-export default async  function getmyToken() {
+export default async function getmyToken() {
 
     const myCokkies = await cookies()
     const myTokenFromCokkies = myCokkies.get("next-auth.session-token")?.value
-    if(myTokenFromCokkies == null){
+    if (myTokenFromCokkies == null) {
         return null
     }
-    const myTokenAfterDecoded = await decode({token : myTokenFromCokkies, secret : process.env.NEXTAUTH_SECRET!})
+    const myTokenAfterDecoded = await decode({ token: myTokenFromCokkies, secret: process.env.NEXTAUTH_SECRET! })
+    if (myTokenAfterDecoded == null) {
+        return null
+    }
     return myTokenAfterDecoded.realtoken
-    
 }
